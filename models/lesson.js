2659fd3ec208babd779ecf931c9c8fdc9e8bd490
Tami Shute <tamaralynnrenee@gmail.com>
2020-05-18 21:31:31 -0400

    lesson model updated

--- models/lesson.js
+++ models/lesson.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 const lessonSchema = new Schema({
   date: { type: Date, default: Date.now },
   class: { type: String, required: true },
-  plan: { topic: String, required: true }
+  plan: [{ topic: String, required: true },
+        {reference: String},
+        {pages: String},
+        {assignment: String}]
 });
 
 const Lesson = mongoose.model("Lesson", lessonSchema);
