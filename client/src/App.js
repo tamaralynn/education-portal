646d5378856a80cbd5b1c05d12e4828d670a5402
Tami Shute <tamaralynnrenee@gmail.com>
2020-06-15 10:09:59 -0400

    template functioning

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+/*import logo from './logo.svg';
 import './App.css';
 import { useFirebaseAuth } from 'use-firebase-auth';
 import SignUp from "./components/SignUp";
@@ -23,4 +23,50 @@ function App() {
     </div>
   );
 }
+export default App; */
+
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { useFirebaseAuth } from "use-firebase-auth";
+
+// core components
+import Admin from "./layouts/Admin";
+import RTL from "./layouts/RTL";
+import SignUp from "./components/SignUp";
+
+import "./assets/css/material-dashboard-react.css";
+
+const hist = createBrowserHistory();
+
+function App() {
+  const {
+    user,
+    loading,
+    error,
+    signInWithProvider,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+  } = useFirebaseAuth();
+  console.log(
+    user,
+    loading,
+    error,
+    signInWithProvider,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword
+  );
+  return (
+    <Router history={hist}>
+      {user && (
+        <Switch>
+          <Route path="/admin" component={Admin} />
+          <Route path="/rtl" component={RTL} />
+          <Redirect from="/" to="/admin/dashboard" />
+        </Switch>
+      )}
+      {!user && <SignUp />}``
+    </Router>
+  );
+}
+
 export default App;
